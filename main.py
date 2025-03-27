ccd1f16f8537408a16dd1049b593adc3b8bcfc00
Luca Matei <dev@lucamatei.eu>
2025-03-27 22:37:45 +0000

    Added comments, exercises

--- main.py
+++ main.py
@@ -1,33 +1,67 @@
 import hashlib
 import random
 
-
+### Python Basics
+number = 123
 boolean = True
-string = "grnegjg"
-float = 123.43
-lista = [1, "2", True, [4], 5, 1]
-tupla = (1, 2, True, 4, 5)
-dictionar = {
+string = "I'm a string"
+float_number = 123.43
+mixed_list = [1, "2", True, [4], 5, 1]  # mutable list
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]  # list of lists / 2D list
+mixed_tuple = (1, 2, True, 4, 5)  # immutable list
+dictionary = {  # key-value pairs
     "key1": 1,
     "key2": "2",
     "key3": True,
     "key4": [4],
-    "key5": 5
+    "key5": 5,
 }
-seturi = {1, 2, True, 4, 5}
+number_set = {1, 2, 3, 4, 5}  # only has unique values
 
+
+### Functions
 def get_random_number(a: int, b: int = 100):
-    """Get a random number between a and b."""
-    # PEP 8 de citit
-    numar: int = random.randint(a, b)
-    return numar
+    """
+    Get a random number between a and b.
+    :param a: lower bound
+    :param b: upper bound (default is 100)
+    """
+    random_number: int = random.randint(a, b)
+    return random_number
 
-print(get_random_number(1, 100))
+
+# Display the random number between 1 and 100
+# print(get_random_number(1, 100))
+
 
 def get_hash(text):
-    """
-    Function to get the hash of a given text.
-    """
+    """Function to get the hash of a given text."""
+    # Hash the text using SHA-256 algorithm
     return hashlib.sha256(text.encode()).hexdigest()
 
-# print(get_hash("numar"))
+
+# Display the hash of the string "Buna, eu sunt un string"
+# print(get_hash("Buna, eu sunt un string"))
+
+
+# Create a function that takes two numbers as arguments and returns their sum here
+...
+
+# Create a function that takes a string as an argument and returns its length here
+...
+
+"""
+Hint: Hover over the function name to see the docstring.
+
+Exercises:
+1. Create a function that takes two numbers as arguments and returns their sum.
+2. Create a function that takes a string as an argument and returns its length. (Hint: Use the len() function)
+3. Commit your newly created functions to Github.
+
+Nice to read:
+- https://peps.python.org/pep-0008/ (PEP 8 - Style Guide for Python Code)
+
+Next time:
+- We'll learn to create a virtual environment (crucial to development)
+- We'll create a game of Snake using Pygame - https://github.com/luca-matei/snake
+"""
